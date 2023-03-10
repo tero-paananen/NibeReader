b3adb6888ec4441890c52d00acab53d4eaa1906a
Tero Paananen <tero.paananen@showell.com>
2023-03-10 15:42:39 +0200

    Some modbus coding

--- NibeConnunication.ts
+++ NibeConnunication.ts
@@ -1,5 +1,62 @@
-const API_KEY = 'YOUR_API_KEY';
+const modbus = require('jsmodbus');
+const net = require('net');
 
-export const login = (username: string, password: string) => {
-  throw Error('Not implemented');
+type NibeData = {tempOutside: string};
+type NibeError = {error?: string; message?: string};
+
+export const readData = async (
+  host: string,
+  port: string
+): Promise<NibeData> => {
+  return new Promise((resolve, reject) => {
+    const socket = new net.Socket();
+
+    const option = {
+      host,
+      port,
+      unitID: 1,
+      timeout: 5000,
+      autoReconnect: true,
+      logLabel: 'Nibe S-series',
+      logLevel: 'error',
+      logEnabled: true,
+    };
+
+    const client = new modbus.client.TCP(socket);
+    socket.connect(option);
+
+    socket.on('connect', async () => {
+      try {
+        console.log('Socket connected');
+        const results = await Promise.all([
+          client.readInputRegisters(1, 1), // BT1 - outside temperature
+        ]);
+        let tempOutside = results[0].response._body._valuesAsBuffer;
+
+        // Convert HEX to Decimal and divide by the scalefactor
+        tempOutside = tempOutside.readInt16BE().toString() / 10; // scale factor is 10
+
+        resolve({tempOutside});
+      } catch (e: any) {
+        console.log('socked read error', e.message);
+        reject(e.message);
+      } finally {
+        socket.end();
+      }
+    });
+
+    socket.on('error', (e: NibeError) => {
+      console.log('Socket closed', getError(e));
+      socket.end();
+      reject(getError(e));
+    });
+
+    socket.on('close', () => {
+      console.log('Socket closed');
+    });
+  });
+};
+
+const getError = (e: NibeError) => {
+  return e.error || e.message || 'Unknown error';
 };
--- README.md
+++ README.md
@@ -9,3 +9,18 @@ https://github.com/elupus/hass_nibe
 https://github.com/x-erik/eu.nibe-modbus
 
 https://www.npmjs.com/package/jsmodbus
+
+
+### Mobus
+
+Reading values via mobus https://www.npmjs.com/package/jsmodbus
+
+Some example 
+
+https://github.com/x-erik/eu.nibe-modbus/blob/821c02a9e919ba7dc21bb324fb52e183e46c3939/drivers/s1155pc/device.js#L65
+
+https://github.com/x-erik/eu.nibe-modbus/blob/821c02a9e919ba7dc21bb324fb52e183e46c3939/drivers/s1155pc/device.js#L81
+
+
+
+
